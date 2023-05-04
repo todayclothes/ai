22276b3f3e10e1d39a9e31bc80f08f4f92ba093d
reeruryu <h2ju1004@gmail.com>
2023-05-04 17:27:56 +0900

    Refactor: clothes api 리팩토링 (#4)
    Co-authored-by: ByeongmokKim <tree199816@gmail.com>
    
    - 가독성을 높이기 위해 중복된 코드 삭제 or 주석 별 정리

--- src/main/model.py
+++ src/main/model.py
@@ -16,61 +16,50 @@ blue_model = Blueprint('model', __name__)
 # parameter : 계절, 성별, 습도, 풍속, 강수, 기온, 스케줄
 @blue_model.route('/clothes', methods=['GET'])
 def get_clothes_recommend():
+    season, gender, humidity, wind_speed, rain, temp = map(float, (request.args.get('season'),
+                                                                            request.args.get('gender'),
+                                                                            request.args.get('humidity'),
+                                                                            request.args.get('wind_speed'),
+                                                                            request.args.get('rain'),
+                                                                            request.args.get('temp')))
+    schedule = request.args.get('schedule')
+    
     try:
-        season = float(request.args.get('season'))
-        gender = float(request.args.get('gender'))
-        humidity = float(request.args.get('humidity'))
-        wind_speed = float(request.args.get('wind_speed'))
-        rain = float(request.args.get('rain'))
-        temp = float(request.args.get('temp'))
-        schedule = request.args.get('schedule')
+        # path
+        model_path = os.path.join(get_current_path(), '..', 'model', 'clothes')
+        ## schedule le path
+        le_schedule_path = os.path.join(model_path, 'le_schedule_top_1.pkl')
+        ## top model path
+        clothes_top_model_path = os.path.join(model_path, 'clothes_top_1.pkl')
+        le_clothes_top_path = os.path.join(model_path, 'le_clothes_top_1.pkl')
+        ## bottom model path
+        clothes_bottom_model_path = os.path.join(model_path, 'clothes_bottom_1.pkl')
+        le_clothes_bottom_path = os.path.join(model_path, 'le_clothes_bottom_1.pkl')
+        
+        # label encoder 선언 및 로드
+        le_clothes_top, le_clothes_bottom, le_schedule = [LabelEncoder() for _ in range(3)]
+        le_clothes_top, le_clothes_bottom = joblib.load(le_clothes_top_path), joblib.load(le_clothes_bottom_path)
+        le_schedule = joblib.load(le_schedule_path) 
+        
+        # 데이터 전처리
+        sample = np.array([season, gender, humidity, wind_speed, rain, temp, schedule]).reshape(-1,1)
+        sample[6] = le_schedule.transform(sample[6])
+        sample = torch.FloatTensor(sample.astype(float).reshape(1,-1))
+        
+        # clothes 예측
+        ## clothes_top 예측
+        clf = joblib.load(clothes_top_model_path)
+        clothes_top = le_clothes_top.inverse_transform(torch.argmax(clf(sample)).cpu().numpy().ravel())
+        ## clothes_bottom 예측
+        clf = joblib.load(clothes_bottom_model_path)
+        clothes_bottom = le_clothes_bottom.inverse_transform(torch.argmax(clf(sample)).cpu().numpy().ravel())
+        
     except ValueError:
         return "Error: Invalid value for 'season' parameter. Must be a float."
     
-    model_path = os.path.join(get_current_path(), '..', 'model')
-    # top model path
-    clothes_top_model_path = os.path.join(model_path, 'clothes', 'clothes_top_1.pkl')
-    le_clothes_top_path = os.path.join(model_path, 'clothes', 'le_clothes_top_1.pkl')
-    le_schedule_top_path = os.path.join(model_path, 'clothes', 'le_schedule_top_1.pkl')
-    
-    # bottom model path
-    clothes_bottom_model_path = os.path.join(model_path, 'clothes', 'clothes_bottom_1.pkl')
-    le_clothes_bottom_path = os.path.join(model_path, 'clothes', 'le_clothes_bottom_1.pkl')
-    le_schedule_bottom_path = os.path.join(model_path, 'clothes', 'le_schedule_bottom_1.pkl')
-    
-    # top le
-    le_clothes_top = LabelEncoder()
-    le_schedule_top = LabelEncoder()
-    
-    # bottom le
-    le_clothes_bottom = LabelEncoder()
-    le_schedule_bottom = LabelEncoder()
-
-    # top le load
-    le_clothes_top = joblib.load(le_clothes_top_path)
-    le_schedule_top = joblib.load(le_schedule_top_path)
-    
-    # bottom le load
-    le_clothes_bottom = joblib.load(le_clothes_bottom_path)
-    le_schedule_bottom = joblib.load(le_schedule_bottom_path)
-    
-    # top classification
-    clf = joblib.load(clothes_top_model_path)
-    sample = np.array([season, gender, humidity, wind_speed, rain, temp, schedule]).reshape(-1,1)    
-    sample[6] = le_schedule_top.transform(sample[6])
-    sample = torch.FloatTensor(sample.astype(float).reshape(1,-1))
-    clothes_top = le_clothes_top.inverse_transform(torch.argmax(clf(sample)).cpu().numpy().ravel())
-    
-    # bottom classification
-    clf = joblib.load(clothes_bottom_model_path)
-    sample = np.array([season, gender, humidity, wind_speed, rain, temp, schedule]).reshape(-1,1)    
-    sample[6] = le_schedule_bottom.transform(sample[6])
-    sample = torch.FloatTensor(sample.astype(float).reshape(1,-1))
-    clothes_bottom = le_clothes_bottom.inverse_transform(torch.argmax(clf(sample)).cpu().numpy().ravel())
-    
     return jsonify({
-        "top" : int(clothes_top[0]),
-        "bottom" : int(clothes_bottom[0])})
+        "top": int(clothes_top[0]),
+        "bottom": int(clothes_bottom[0])})
  
 @blue_model.route('/schedule', methods=['GET'])
 def region_model():
